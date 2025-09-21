402102c9e5064a4760418cb56ba74181795e70ed
samradhyadav <reach.samradhyadav@gmail.com>
2025-09-21 14:45:48 +0530

    Update users.js

--- frontend/src/api/users.js
+++ frontend/src/api/users.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5001/api/users";
+const API_URL = "https://smartordersystems.onrender.com/api/users";
 
 export const fetchUser = async (userId) => {
   const res = await axios.get(`${API_URL}/${userId}`);
