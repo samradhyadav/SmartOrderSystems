e8da0971769d2284d54bc767a9e9b2deac66d93a
samradhyadav <reach.samradhyadav@gmail.com>
2025-09-21 15:22:52 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { API_BASE_URL } from "../../config.js";
+import { API_BASE_URL } from "../config.js";
 
 const Login = () => {
   const [email, setEmail] = useState("");
